fc4b6936b7e5f9df9e18c08185f0bf1c5a52f4ea
chesha1 <49867392+chesha1@users.noreply.github.com>
2023-01-20 19:08:28 +0800

    add more models to choose

--- stable_diffusion_t2i.py
+++ stable_diffusion_t2i.py
@@ -11,19 +11,21 @@ import torch
 # torch.backends.cuda.matmul.allow_tf32 = True
 
 # model_id = "stabilityai/stable-diffusion-2-1"
-model_id = "../stable-diffusion-v1-5"
+# model_id = "../stable-diffusion-2-1"
+# model_id = "../stable-diffusion-v1-5"
+model_id = "../anything-v4.0"
 device = torch.device("cuda")
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to(device)
 
-prompt = "anime girl, beautiful, big breast, blue eyes, long hair, masterpiece"
+prompt = "masterpiece, best quality, 1girl, white hair, medium hair, cat ears, closed eyes, looking at viewer, :3, cute, scarf, jacket, outdoors, streets"
 neg_prompt = "bad fingers"
 num_inference_steps = 50
-height = 256
-width = 256
-num_images_per_prompt = 3
+height = 512
+width = 512
+num_images_per_prompt = 1
 guidance_scale = 7.5
 num_times = 1
 
