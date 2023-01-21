bb10c535c4642ec8689b15bbccd0147c5e05a24c
chesha1 <49867392+chesha1@users.noreply.github.com>
2023-01-21 10:12:51 +0800

    add dreambooth test scripts

--- DreamBooth/inference_from_ckpt.py
+++ DreamBooth/inference_from_ckpt.py
@@ -4,9 +4,35 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from accelerate import Accelerator
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+import argparse
+
+parser = argparse.ArgumentParser(description="DreamBooth utils")
+parser.add_argument(
+        "--pretrained_model_name_or_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+parser.add_argument(
+        "--checkpoint_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to checkpoint of DreamBooth",
+    )
+parser.add_argument(
+        "--save_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to unwrapped diffusers model",
+    )
+args = parser.parse_args()
 
 # Load the pipeline with the same arguments (model, revision) that were used for training
-model_id = "/home/models/anything-v4.0"
+# model_id = "/home/models/anything-v4.0"
+model_id = args.pretrained_model_name_or_path
 pipeline = DiffusionPipeline.from_pretrained(model_id)
 pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 accelerator = Accelerator()
@@ -15,7 +41,8 @@ accelerator = Accelerator()
 unet, text_encoder = accelerator.prepare(pipeline.unet, pipeline.text_encoder)
 
 # Restore state from a checkpoint path. You have to use the absolute path here.
-accelerator.load_state("/home/output/checkpoint-1000")
+# accelerator.load_state("/home/output/checkpoint-1000")
+accelerator.load_state(args.checkpoint_path)
 
 # Rebuild the pipeline with the unwrapped models (assignment to .unet and .text_encoder should work too)
 pipeline = DiffusionPipeline.from_pretrained(
@@ -25,4 +52,5 @@ pipeline = DiffusionPipeline.from_pretrained(
 )
 
 # Perform inference, or save, or push to the hub
-pipeline.save_pretrained("dreambooth_pipeline_1000")
+# pipeline.save_pretrained("dreambooth_pipeline_1000")
+pipeline.save_pretrained(args.save_path)
--- DreamBooth/stable_diffusion_t2i.py
+++ DreamBooth/stable_diffusion_t2i.py
@@ -5,15 +5,34 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
+import argparse
 
 # make speed lower
 # torch.backends.cudnn.benchmark = True
 # torch.backends.cuda.matmul.allow_tf32 = True
 
+parser = argparse.ArgumentParser(description="DreamBooth utils")
+parser.add_argument(
+        "--pretrained_model_name_or_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+parser.add_argument(
+        "--save_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to save images",
+    )
+args = parser.parse_args()
+
 # model_id = "stabilityai/stable-diffusion-2-1"
 # model_id = "../stable-diffusion-2-1"
 # model_id = "../stable-diffusion-v1-5"
-model_id = "../anything-v4.0"
+# model_id = "../anything-v4.0"
+model_id = args.pretrained_model_name_or_path
 device = torch.device("cuda")
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -42,4 +61,5 @@ for i in range(num_times):
     print('Generate {} successfully.'.format(str(i)))
 
     for j in range(num_images_per_prompt):
-        images[j].save("../images/image_{}_{}.png".format(str(seed), str(j)))
+        # images[j].save("../images/image_{}_{}.png".format(str(seed), str(j)))
+        images[j].save(args.save_path.format(str(seed), str(j)))
