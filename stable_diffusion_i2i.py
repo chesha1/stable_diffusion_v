b2e7fc15c30ad52be95ab2dffacd05485bbb9d20
chesha1 <49867392+chesha1@users.noreply.github.com>
2023-01-27 20:14:58 +0800

    i2i strength list

--- stable_diffusion_i2i.py
+++ stable_diffusion_i2i.py
@@ -11,33 +11,35 @@ from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepSchedule
 
 # load the pipeline
 device = "cuda"
-model_id = "../anything-v4.0"
+model_id = "/home/models/model4"
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to(device)
 
-init_image = Image.open("../images/image_2950047911628730.png")
+init_image = Image.open("/home/images/test.png")
 width, height = init_image.size
-init_image = init_image.resize((768, 768))
+# init_image = init_image.resize((768, 768))
 
 
-prompt = "masterpiece, best quality, 1girl, white hair, medium hair, cat ears, closed eyes, looking at viewer, :3, cute, scarf, jacket, outdoors, streets"
-neg_prompt = "bad fingers"
-strength = 0.6
+prompt = "((masterpiece)), (((best quality))), ((ultra-detailed)), ((illustration)), stage, concert, a lot of waving glow sticks, performance, audience, lights, ((1 girl)), (((((lun))))), pink hair, light green eyes, (hands behind back), (solo), ((singing)), ((leaning_forward)), (arms_behind_back), ((extremely_detailed_eyes_and_face)), colorful, Tokyo Dome, ray tracing"
+neg_prompt = "2 girl, an incomplete face, a contorted face, bad anatomy, ((bad hands)), lowres, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, (missing fingers), bad hands, long neck, Humpbacked, extra legs"
+
+strength = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 guidance_scale = 7.5
-num_images_per_prompt = 1
+num_images_per_prompt = 3
 num_inference_steps = 50
-num_times = 1
+num_times = 4
 
-for i in range(num_times):
-    generator = torch.Generator(device=device)
-    seed = generator.seed()
-    generator = generator.manual_seed(seed)
+for st in strength:
+    for i in range(num_times):
+        generator = torch.Generator(device=device)
+        seed = generator.seed()
+        generator = generator.manual_seed(seed)
 
-    PipelineOut = pipe(prompt=prompt, negative_prompt=neg_prompt, image=init_image, num_inference_steps=num_inference_steps,
-                       strength=strength, guidance_scale=guidance_scale, generator=generator,
-                       num_images_per_prompt=num_images_per_prompt)
-    images = PipelineOut.images
-    print('Generate {}/{} successfully.'.format(str(i), str(num_times)))
-    for j in range(num_images_per_prompt):
-        images[j].save("../images/image_{}_{}.png".format(str(seed), str(j)))
+        PipelineOut = pipe(prompt=prompt, negative_prompt=neg_prompt, image=init_image, num_inference_steps=num_inference_steps,
+                           strength=strength, guidance_scale=guidance_scale, generator=generator,
+                           num_images_per_prompt=num_images_per_prompt)
+        images = PipelineOut.images
+        print('Generate {}/{} successfully.'.format(str(i+1), str(num_times)))
+        for j in range(num_images_per_prompt):
+            images[j].save("../images/image_{}_{}_{}.png".format(st, str(seed), str(j)))
